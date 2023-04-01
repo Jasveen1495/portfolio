614f308cfe5a52f934a9ae08c0b721b7b61a1c20
kushdeepwalia <skushdeep5@gmail.com>
2023-04-01 17:00:26 +0530

    error removed

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Intro from '../../components/Intro'
 import Achievement from '../../components/Achievement'
 import Contact from '../../components/Contact'
